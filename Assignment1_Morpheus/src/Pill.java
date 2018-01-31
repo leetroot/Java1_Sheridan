4f329e733962575861c7527ee1389e1cacce57a3
leetroot <jami@sheridancollege.ca>
2018-01-31 16:20:26 -0500

    Added Morpheus and Pill java files, also opened Assignment 1 on GitHub

---
+++ Assignment1_Morpheus/src/Pill.java
@@ -0,0 +1,12 @@
+public class Pill
+{
+    private String color;
+    private double size;
+    private String efficacy;
+    private boolean status;
+
+    public Pill(String color, double size)
+    {
+
+    }
+}
