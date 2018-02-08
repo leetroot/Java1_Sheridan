1e7d24fd8d7f4ba13fbae207fc9a694ee0e5e6e4
leetroot <jami@sheridancollege.ca>
2018-02-08 13:15:53 -0500

    FINAL ASSIGNMENT 1
    - Added in Morpheus and Pill to reflect assignment deliverables.
    - Debugged and tested conditional statements to prevent double input
    - Cleaned up code and removed fuzzy stuff.
    - Optimized and added final comments.
    - Instead of generic messages, replaced it with a random message generator
    to add a little creativity ;)
    - Changed some variable names to make it more meaningful.
    
    Signed-off-by: leetroot <jami@sheridancollege.ca>

--- Assignment1_Morpheus/src/Morpheus.java
+++ Assignment1_Morpheus/src/Morpheus.java
@@ -1,52 +1,100 @@
+/*
+ * Name: Humza Jami
+ * Class: PROG23199 1171_3093, Winter 2018
+ * Assignment: Assignment #1 Meeting Morpheus
+ * Date: February 8, 2018
+ * Program: Morpheus.java
+ * Github: https://github.com/leetroot/Java1_Sheridan/tree/master/Assignment1_Morpheus/src/Morpheus.java
+ */
 import java.util.*;
-
 
 public class Morpheus
 {
     public static void main(String[] args)
     {
-        int option;
-        Pill bluePill = new Pill("Blue");
-        Pill redPill = new Pill("Red");
-
-        bluePill.setSize(1.414);
-        redPill.setSize(1.414);
-
-        bluePill.setStatus(false);
-        redPill.setStatus(false);
+        String[] outputResponses = {"Neo, think harder", "Think carefully about your decision. Try again.", "Don't disappoint me, I know you want to go in", "I know this isn't easy but make the right choice."}; //Create a new array of computer responses.
 
 
-        double bluePillSize = bluePill.getSize();
-        boolean bluePillStatus = bluePill.getStatus();
+        int option; //Create a new number to read user input from
+        int yesOrNoReply; //Create a new number to read user input from
+        Pill bluePill = new Pill("Blue"); //Creating a new pill with color blue
+        Pill redPill = new Pill("Red"); //Creating a new pill with color red
 
-        double redPillSize = redPill.getSize();
-        boolean redPillStatus = redPill.getStatus();
+        bluePill.setSize(1.414); //Setting the size of blue pill
+        redPill.setSize(1.414); //Setting the size of red pill
 
-        String redPillEfficacy = redPill.setEfficacy("Tracing for unplugging this human");
-        String bluePillEfficacy = bluePill.setEfficacy("Sending back to the matrix to sleep");
+        bluePill.setStatus(false); //Sets the blue pill status to false
+        redPill.setStatus(false); //Sets the red pill status to false.
 
-        System.out.println("Morpheus: Unfortunately, no one can be...told what the matrix is. You have");
-        System.out.println("to see it for yourself. This is your last chance. After this, there is no ");
-        System.out.println("turning back. I have two pills here: ");
-        System.out.println("\t\t == Pill One ==");
-        System.out.println("\t\t Color: \t\tRed");
-        System.out.println("\t\t Size: \t\t\t" + redPillSize);
-        System.out.println("\t\t Efficacy: \t\tTracing for unplugging this human");
-        System.out.println("\t\t Status: \t\tAvailable");
 
-        System.out.println("\t\t == Pill Two ==");
-        System.out.println("\t\t Color: \t\tBlue");
-        System.out.println("\t\t Size: \t\t\t" + bluePillSize);
-        System.out.println("\t\t Efficacy: \t\tSending back to the Matrix to sleep");
-        System.out.println("\t\t Status: \t\tAvailable");
+        double bluePillSize = bluePill.getSize(); //Gets the size of blue pill
+        boolean bluePillStatus = bluePill.getStatus(); //Gets the status of blue pill
 
-        System.out.println("You take the blue pill (Press 1), the story ends, you wake up in your bed");
-        System.out.println("and believe whatever you want to believe. You take the red pill (Press 2),");
-        System.out.println("you stay in Wonderland, and I show you how deep the rabbit hole goes.");
+        double redPillSize = redPill.getSize(); //Gets the size of red pill
+        boolean redPillStatus = redPill.getStatus(); //Gets the status of red pill
 
-        Scanner input = new Scanner(System.in);
-        System.out.println("Morpheus: Now tell me Neo, (1) Blue or (2) Red?");
-        option = input.nextInt();
+        String redPillEfficacy = redPill.setEfficacy("Tracing for unplugging this human"); //Set efficacy message for red pill
+        String bluePillEfficacy = bluePill.setEfficacy("Sending back to the matrix to sleep"); //Setting efficacy message for blue pill
 
+        System.out.println("Morpheus: Unfortunately, no one can be...told what the matrix is. You have"); //Prints out a message
+        System.out.println("to see it for yourself. This is your last chance. After this, there is no "); //Prints out a message
+        System.out.println("turning back. I have two pills here: "); //Prints out a message
+        System.out.println("\t\t == Pill One =="); //Prints out a message
+        System.out.println("\t\t Color: \t\tRed"); //Prints out a message
+        System.out.println("\t\t Size: \t\t\t" + redPillSize); //Prints out the size of red pill
+        System.out.println("\t\t Efficacy: \t\t" + redPillEfficacy); //Prints out the efficacy of red pill
+        System.out.println("\t\t Status: \t\t " + redPillStatus); //Prints out the status of red pill
+        System.out.println(); //Prints out a new line
+        System.out.println("\t\t == Pill Two =="); //Prints out a message
+        System.out.println("\t\t Color: \t\tBlue"); //Prints out a message
+        System.out.println("\t\t Size: \t\t\t" + bluePillSize); //Prints out the size of blue pill
+        System.out.println("\t\t Efficacy: \t\t" + bluePillEfficacy); //Prints out the efficacy of blue pill
+        System.out.println("\t\t Status: \t\t" + bluePillStatus); //Prints out the status of blue pill
+
+        System.out.println("You take the blue pill (Press 1), the story ends, you wake up in your bed"); //Prints out a message
+        System.out.println("and believe whatever you want to believe. You take the red pill (Press 2),"); //Prints out a message;
+        System.out.println("you stay in Wonderland, and I show you how deep the rabbit hole goes."); //Prints out a message
+
+        Scanner input = new Scanner(System.in); //Creates a new user input scanner
+        Scanner yesOrNo = new Scanner(System.in); //Creates a new user input scanner
+        System.out.println("Morpheus: Now tell me Neo, (1) Blue or (2) Red?"); //Asks user to choose a option
+        option = input.nextInt(); //Reads user input
+
+        /**
+         * While option isn't set to 0, the program checks for user input, the program will generate a random response
+         * if user presses 1. If user enters 2, the system will ask the user to make a decision, if user presses 1,
+         * the user will be displayed a Morpheus message and then the system will close the input and exit the program.
+         * If user selects 2, Morpheus will say two things and then require the user to make a choice again.
+         */
+        while(option != 0)
+        {
+            switch (option)
+            {
+                case 1:
+                    int computerResponse = new Random().nextInt(outputResponses.length);
+                    System.out.println("Morpheus: " + outputResponses[computerResponse]);
+                    System.out.println("Morpheus: Now tell me Neo, blue (1) or red (2)");
+                    option = input.nextInt();
+                    break;
+
+
+                case 2:
+                    System.out.println("Morpheus: Are you sure, (1) Yes or (2) No? Remember, all I'm offering is the truth. Nothing more. ");
+                    yesOrNoReply = yesOrNo.nextInt();
+                    if(yesOrNoReply == 1)
+                    {
+                        System.out.println("Morpheus: Follow Me.");
+                        System.out.close();
+                        System.exit(0);
+                    }
+
+                    else if(yesOrNoReply == 2)
+                    {
+                        System.out.println("Morpheus: Ok, think about it then decide. ");
+                        System.out.println("Morpheus: Now tell me Neo, blue (1) or red (2)");
+                        option = input.nextInt();
+                    }
+            }
+        }
     }
 }
--- Assignment1_Morpheus/src/Pill.java
+++ Assignment1_Morpheus/src/Pill.java
@@ -1,3 +1,12 @@
+/*
+ * Name: Humza Jami
+ * Class: PROG23199 1171_3093, Winter 2018
+ * Assignment: Assignment #1 Meeting Morpheus
+ * Date: February 8, 2018
+ * Program: Pill.java
+ * Github: https://github.com/leetroot/Java1_Sheridan/tree/master/Assignment1_Morpheus/src/Pill.java
+ */
+
 public class Pill
 {
     private String color;
@@ -11,52 +20,93 @@ public class Pill
         size = 2.0;
     }
 
+    /**
+     * Gets color by grabbing the pre set color from the constructor.
+     * @return Calling the constructor to get color of the pill
+     */
     public String getColor()
     {
         return this.color;
     }
 
+    /**
+     * Setting the color of the pill by overriding the constructors
+     * @param newColor replacing the constructor's color with the color called in through the setter
+     */
     public void setColor(String newColor)
     {
         this.color = newColor;
     }
 
+    /**
+     * Gets size by checking the constructor
+     * @return Callign constructor to get size of pill
+     */
     public double getSize()
     {
         return this.size;
     }
 
+    /**
+     * Sets the size of the pill by overriding Pill constructor
+     * @param newSize replacing the size from constructor with newSize
+     */
     public void setSize(double newSize)
     {
         this.size = newSize;
     }
 
+    /**
+     *
+     * @param efficacy sets efficacy by calling in the setter method
+     * @return the value of efficacy since a void method is called.
+     */
     public String setEfficacy(String efficacy)
     {
         this.efficacy = efficacy;
         return efficacy;
     }
 
+    /**
+     * Gets efficacy from the private String
+     * @return Grab efficacy from Pill.
+     */
     public String getEfficacy()
     {
         return this.efficacy;
     }
 
+    /**
+     * Checks if the pill is available or not.
+     * @param status sets the default value
+     * @return the status of the pill
+     */
     public boolean isTaken(boolean status)
     {
         return this.status;
     }
 
+    /**
+     * Sets the status of pill by defining a boolean value
+     * @param status sets method status in place of Pill constructor status.
+     */
     public void setStatus(boolean status)
     {
         this.status = status;
     }
 
+    /**
+     * Gets pill status
+     * @return Grab status from method setStatus
+     */
     public boolean getStatus()
     {
         return this.status;
     }
 
+    /**
+     * Print info of pill based on size, status and efficacy
+     */
     public void printInfo()
     {
         if(size < 2.0)
