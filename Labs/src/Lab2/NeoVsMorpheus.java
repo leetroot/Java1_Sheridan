0cf11a9f756e6e0754cc1dd703597cd8f4fb3699
leetroot <jami@sheridancollege.ca>
2018-02-15 15:44:36 -0500

    Added in File Headers to sign off own work. - Lab 2 is complete.
    Signed-off-by: leetroot <jami@sheridancollege.ca>

--- Labs/src/Lab2/NeoVsMorpheus.java
+++ Labs/src/Lab2/NeoVsMorpheus.java
@@ -1,3 +1,12 @@
+/*
+ * Name: Humza Jami
+ * Class: PROG23199 1171_3093, Winter 2018
+ * Assignment: Lab #2 Neo Vs. Morpheus
+ * Date: February 15, 2018
+ * Program: NeoVsMorpheus.java
+ * Assignment GitHub URL: https://github.com/leetroot/Java1_Sheridan/blob/master/Labs/src/Lab2/NeoVsMorpheus.java
+ */
+
 package Lab2;
 
 //import SecureRandom and Scanner classes
--- Labs/src/Lab2/Trainee.java
+++ Labs/src/Lab2/Trainee.java
@@ -1,3 +1,13 @@
+/*
+ * Name: Humza Jami
+ * Class: PROG23199 1171_3093, Winter 2018
+ * Assignment: Lab #2 Neo Vs. Morpheus
+ * Date: February 15, 2018
+ * Program: Trainee.java
+ * Assignment GitHub URL: https://github.com/leetroot/Java1_Sheridan/blob/master/Labs/src/Lab2/NeoVsMorpheus.java
+ */
+
+
 package Lab2;
 
 public class Trainee {
